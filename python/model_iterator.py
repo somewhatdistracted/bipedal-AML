1a66a9be6bbfd1c296f61d90d36abc3f606226bc
Ian Patrick MacFarlane <ian@macfarlanehome.com>
2019-06-02 16:59:50 -0700

    Made the nasty for model_iterator, but norm is a thing

--- python/model_iterator.py
+++ python/model_iterator.py
@@ -13,6 +13,10 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+def norm(x):
+    n = read_csv("norm.csv")
+    return (x - n['mean']) / n['std']
+
 def getModel():
     print("Loading Model from JSON...   ", end = "")
     json_file = open("preTrainedModel.json", "r")
@@ -39,6 +43,6 @@ def permuteModel(model):
 def runModel(model, data):
     t = pd.DataFrame(data, columns = ['RHipAngleX', 'RHipAngleY', 'RHipAngleZ', 'LHipAngleX', 'LHipAngleY', 'LHipAngleZ',
     'RKneeAngleZ', 'LKneeAngleZ', "RAnkleAngleX", "RAnkleAngleZ", "LAnkleAngleX", "LAnkleAngleZ"])
-    t.rename(columns = {0 : 'RHipAngleX', 1 : 'RHipAngleY', 2 : 'RHipAngleZ', 3 : 'LHipAngleX', 4 : 'LHipAngleY', 5 : 'LHipAngleZ',
-    6 : 'RKneeAngleZ', 7 : 'LKneeAngleZ', 8 : "RAnkleAngleX", 9 : "RAnkleAngleZ", 10 : "LAnkleAngleX", 11 : "LAnkleAngleZ"}, inplace = True)
-    return model.predict(t)
+    return model.predict(norm(t))
+
+#print(runModel(getModel(), [(1,1,1,1,1,1,1,1,1,1,1,1)])) <---- here is the form for data input
