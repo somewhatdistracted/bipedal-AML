f9f25e0778261f6500e67925d711421dedf51e93
Ian Patrick MacFarlane <ian@macfarlanehome.com>
2019-06-02 14:16:05 -0700

    Updated model_iterator

--- python/model_iterator.py
+++ python/model_iterator.py
@@ -14,16 +14,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import random
 
 def getModel():
     print("Loading Model from JSON...   ", end = "")
     json_file = open("preTrainedModel.json", "r")
     model_json = json_file.read()
     json_file.close()
-    model = model_from_json(model_json)
+    model = tf.keras.models.model_from_json(model_json)
     model.load_weights("preTrainedModel_weights.h5")
     print("Loaded.")
     return model
 
-# def permuteModel(model):
-#
+def permuteModel(model):
+    weights = np.array(model.get_weights())
+    for i in range(weights.shape[0]):
+        x, *y = weights[i].shape
+        if len(y) == 0:
+            z = 1
+        else:
+            z = y[0]
+        delta = np.random.rand(x,z)
+        weights[i] = weights[i] + delta
+    model.set_weights(weights)
+    return model
+
+permuteModel(getModel())
