278972b88568fdf55b32391a5d21375661d5b613
Ian Patrick MacFarlane <ian@macfarlanehome.com>
2019-06-03 00:45:33 -0700

    gogogo

--- python/model_iterator.py
+++ python/model_iterator.py
@@ -14,10 +14,28 @@ import pandas as pd
 import numpy as np
 
 def norm(x):
-    indices = ['RHipAngleX', 'RHipAngleY', 'RHipAngleZ', 'LHipAngleX', 'LHipAngleY', 'LHipAngleZ',
+    column_names = ['RHipAngleX', 'RHipAngleY', 'RHipAngleZ', 'LHipAngleX', 'LHipAngleY', 'LHipAngleZ',
     'RKneeAngleZ', 'LKneeAngleZ', "RAnkleAngleX", "RAnkleAngleZ", "LAnkleAngleX", "LAnkleAngleZ"]
-    n = pd.read_csv("norm.csv", index_col = indices, names = ['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
-    return (x - n['mean']) / n['std']
+    raw = pd.read_csv('allsubject.csv', names = column_names)
+    dataset = raw.copy()
+    labelledset = dataset
+    temp = labelledset.drop([0])
+    labelledset = temp.append(labelledset[:1]).reset_index(drop = True)
+    dataset = pd.concat([dataset, labelledset], axis = 1)
+    train_dataset = dataset.sample(frac=0.8,random_state=0)
+    test_dataset = dataset.drop(train_dataset.index)
+    train_labels = train_dataset.iloc[:, 12:]
+    train_dataset = train_dataset.iloc[:, :12]
+    test_labels = test_dataset.iloc[:, 12:]
+    test_dataset = test_dataset.iloc[:, :12]
+    train_stats = train_dataset.describe()
+    train_stats = train_stats.transpose()
+    return (x - train_stats['mean']) / train_stats['std']
+
+    #indices = ['RHipAngleX', 'RHipAngleY', 'RHipAngleZ', 'LHipAngleX', 'LHipAngleY', 'LHipAngleZ',
+    #'RKneeAngleZ', 'LKneeAngleZ', "RAnkleAngleX", "RAnkleAngleZ", "LAnkleAngleX", "LAnkleAngleZ"]
+    #n = pd.read_csv("norm.csv", index_col = indices, names = ['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
+    #return (x - n['mean']) / n['std']
 
 def getModel():
     print("Loading Model from JSON...   ", end = "")
